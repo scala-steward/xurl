7dec5579a47999e2e4dca0978e9f66d6e650b2ee
Scala Steward <scala_steward@virtuslab.com>
2026-04-15 15:18:25 +0000

    Update play-json to 3.0.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val derevo           = "0.14.0"
     val log4cats         = "2.8.0"
     val skunk            = "0.6.5"
-    val playJson         = "3.0.4"
+    val playJson         = "3.0.6"
     val redis            = "2.0.3"
     val http4sPrometheus = "0.25.0"
     val http4s           = "0.23.34"
@@ -22,25 +22,25 @@ object Dependencies {
   }
 
   object libraries {
-    val cats                = "org.typelevel"         %% "cats-core"                 % versions.cats
-    val catsEffect          = "org.typelevel"         %% "cats-effect"               % versions.catsEffect
-    val newtype             = "io.estatico"           %% "newtype"                   % versions.newtype
-    val refined             = "eu.timepit"            %% "refined"                   % versions.refined
-    val derevoCore          = "tf.tofu"               %% "derevo-core"               % versions.derevo
-    val derevoCats          = "tf.tofu"               %% "derevo-cats"               % versions.derevo
-    val log4cats            = "org.typelevel"         %% "log4cats-slf4j"            % versions.log4cats
-    val slf4jSimple         = "org.slf4j"              % "slf4j-simple"              % versions.slf4j
-    val skunk               = "org.tpolecat"          %% "skunk-core"                % versions.skunk
-    val redis               = "dev.profunktor"        %% "redis4cats-effects"        % versions.redis
-    val redisL4C            = "dev.profunktor"        %% "redis4cats-log4cats"       % versions.redis
-    val playJson            = "org.playframework"     %% "play-json"                 % versions.playJson
-    val playJsonDerived     = "org.julienrf"          %% "play-json-derived-codecs"  % "11.0.0"
-    val http4sDsl            = "org.http4s"            %% "http4s-dsl"                % versions.http4s
-    val http4sEmberServer    = "org.http4s"            %% "http4s-ember-server"       % versions.http4s
-    val http4sEmberClient    = "org.http4s"            %% "http4s-ember-client"       % versions.http4s
-    val http4sPlayJson       = "org.http4s"            %% "http4s-play-json"          % "0.23.15"
-    val http4sPrometheus     = "org.http4s"            %% "http4s-prometheus-metrics" % versions.http4sPrometheus
-    val pureConfig           = "com.github.pureconfig" %% "pureconfig"                % versions.pureConfig
+    val cats              = "org.typelevel"         %% "cats-core"                 % versions.cats
+    val catsEffect        = "org.typelevel"         %% "cats-effect"               % versions.catsEffect
+    val newtype           = "io.estatico"           %% "newtype"                   % versions.newtype
+    val refined           = "eu.timepit"            %% "refined"                   % versions.refined
+    val derevoCore        = "tf.tofu"               %% "derevo-core"               % versions.derevo
+    val derevoCats        = "tf.tofu"               %% "derevo-cats"               % versions.derevo
+    val log4cats          = "org.typelevel"         %% "log4cats-slf4j"            % versions.log4cats
+    val slf4jSimple       = "org.slf4j"              % "slf4j-simple"              % versions.slf4j
+    val skunk             = "org.tpolecat"          %% "skunk-core"                % versions.skunk
+    val redis             = "dev.profunktor"        %% "redis4cats-effects"        % versions.redis
+    val redisL4C          = "dev.profunktor"        %% "redis4cats-log4cats"       % versions.redis
+    val playJson          = "org.playframework"     %% "play-json"                 % versions.playJson
+    val playJsonDerived   = "org.julienrf"          %% "play-json-derived-codecs"  % "11.0.0"
+    val http4sDsl         = "org.http4s"            %% "http4s-dsl"                % versions.http4s
+    val http4sEmberServer = "org.http4s"            %% "http4s-ember-server"       % versions.http4s
+    val http4sEmberClient = "org.http4s"            %% "http4s-ember-client"       % versions.http4s
+    val http4sPlayJson    = "org.http4s"            %% "http4s-play-json"          % "0.23.15"
+    val http4sPrometheus  = "org.http4s"            %% "http4s-prometheus-metrics" % versions.http4sPrometheus
+    val pureConfig        = "com.github.pureconfig" %% "pureconfig"                % versions.pureConfig
 
     val weaverCats       = "org.typelevel" %% "weaver-cats"       % versions.weaver
     val weaverDiscipline = "org.typelevel" %% "weaver-discipline" % versions.weaver
